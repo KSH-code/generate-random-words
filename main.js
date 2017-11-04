dca733ac058b3df52eb552b81446de91447a9610
SeongHoon Kim <tjdgnsqn133@gmail.com>
2017-11-04 13:21:04 +0900

    Initial Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const generatedWord = require('./generateWord');
+(function() {
+    console.log(generatedWord());
+    console.log(generatedWord());
+    console.log(generatedWord());
+    console.log(generatedWord());
+})();
